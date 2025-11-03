92f524e10c29c53563ac85ac1c208e1c9ba42efd
nicolas.moreno <nicolas.moreno@sngular.com>
2025-11-03 13:35:28 +0100

    Replace HomePage with Layout and move navigation links to Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Anecdotes from './pages/Anecdotes';
-import HomePage from './pages/HomePage';
+import Layout from './pages/Layout';
 import Counter from './pages/Counter';
 import LeftRight from './pages/LeftRight';
 import ListaCompra from './pages/ListaCompra';
 import Part1 from './pages/Part1';
 import Unicafe from './pages/Unicafe';
+import HomePage from './pages/HomePage';
 
 function App() {
 
   return (
     <BrowserRouter>
-      <HomePage />
+      <Layout />
 
       <hr />
       <Suspense fallback={<div>Cargando proyecto...</div>}>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/anecdotes" element={<Anecdotes />} />
           <Route path="/left-right" element={<LeftRight />} />
           <Route path="/lista-compra" element={<ListaCompra />} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-
 const HomePage = () => {
-
-    const padding = {
-        padding: 5
-    };
-
     return (
-        <div>
-            <h1>React Example App</h1>
-            <nav>
-                <Link style={padding} to="/anecdotes">Anecdotes</Link>
-                <Link style={padding} to="/left-right">Left-Right</Link>
-                <Link style={padding} to="/lista-compra">Lista Compra</Link>
-                <Link style={padding} to="/part1">Part 1</Link>
-                <Link style={padding} to="counter">Counter</Link>
-                <Link style={padding} to="/unicafe">Unicafe</Link>
-            </nav>
-        </div >
+        <></>
     )
 }
 
