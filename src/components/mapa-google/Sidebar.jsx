f0a8b8b6df58c7d6ce603720350b99f869cb7b90
nicolas.moreno <nicolas.moreno@sngular.com>
2025-12-17 14:31:18 +0100

    Add sidebar and update styles

---
+++ src/components/mapa-google/Sidebar.jsx
@@ -0,0 +1,16 @@
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <h2>Marcadores</h2>
+        <p>Descripción</p>
+      </div>
+      <div className="sidebar-content">
+        <h3>Lugar</h3>
+        <p>Dirección</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
