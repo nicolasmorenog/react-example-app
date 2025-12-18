5fc79ad6c3b898f16fadb0efe6acb7e428d1111c
nicolas.moreno <nicolas.moreno@sngular.com>
2025-12-18 12:20:20 +0100

    Refactor MapView component to remove unused imports and streamline map ID usage

--- src/components/mapa-google/MapView.jsx
+++ src/components/mapa-google/MapView.jsx
@@ -1,15 +1,18 @@
-//import React, { useState, useCallback } from 'react';
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const center = {
+  // coordenadas de Madrid
   lat: 40.416775,
   lng: -3.70379,
 };
+
+const MY_MAP_ID = 'e3e91f42e60dbb148da20095';
 
 const MapView = () => {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    mapIds: [MY_MAP_ID],
   });
 
   if (!isLoaded) return <div className="map">Cargando mapa...</div>;
@@ -19,13 +22,12 @@ const MapView = () => {
       mapContainerClassName="map"
       center={center}
       zoom={10}
+      mapId={MY_MAP_ID}
       options={{
         streetViewControl: false,
         mapTypeControl: false,
       }}
-    >
-      <Marker position={center} />
-    </GoogleMap>
+    ></GoogleMap>
   );
 };
 
