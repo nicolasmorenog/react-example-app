a851817ef08fb341f21cbe9d9c11224129b239c5
nicolas.moreno <nicolas.moreno@sngular.com>
2025-12-22 12:57:13 +0100

    add InfoWindow to display airport details and update CSS for light mode

--- src/components/mapa-google/MapView.jsx
+++ src/components/mapa-google/MapView.jsx
@@ -1,20 +1,23 @@
 'use client';
 
-import { APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
-
-const center = {
-  // coordenadas de Madrid
-  lat: 40.416775,
-  lng: -3.70379,
-};
-
-const airport = {
-  // coordenadas del Aeropuerto de Adolfo Suárez Madrid-Barajas
-  lat: 40.4719,
-  lng: -3.5526,
-};
+import { useState } from 'react';
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from '@vis.gl/react-google-maps';
 
 const MapView = ({ isDarkMode }) => {
+  const center = {
+    // coordenadas de Madrid
+    lat: 40.416775,
+    lng: -3.70379,
+  };
+
+  const airport = {
+    // coordenadas del Aeropuerto Adolfo Suárez Madrid-Barajas
+    lat: 40.4719,
+    lng: -3.5526,
+  };
+
+  const [open, setOpen] = useState(false);
+
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <div className="map-frame">
@@ -24,9 +27,15 @@ const MapView = ({ isDarkMode }) => {
           defaultCenter={center}
           colorScheme={isDarkMode ? 'DARK' : 'LIGHT'}
         >
-          <AdvancedMarker position={airport}>
+          <AdvancedMarker position={airport} onClick={() => setOpen(true)}>
             <Pin background={'black'} borderColor={'white'} glyphColor={'white'} />
           </AdvancedMarker>
+
+          {open && (
+            <InfoWindow className="infowindow-content" position={airport} onCloseClick={() => setOpen(false)}>
+              <h3>Aeropuerto Adolfo Suárez Madrid-Barajas</h3>
+            </InfoWindow>
+          )}
         </Map>
       </div>
     </APIProvider>
